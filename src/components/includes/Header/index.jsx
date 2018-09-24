943ee3fc5f44be9b691b50f2d1c4ee8e761c9051
Ismail Ghallou <ismai23l@hotmail.com>
2018-09-24 11:26:04 +0100

    Update index.jsx

--- src/components/includes/Header/index.jsx
+++ src/components/includes/Header/index.jsx
@@ -7,14 +7,14 @@ import logo from '../../../assets/logo.svg'
 import logout from '../../../assets/logout.svg'
 import './style.css'
 
-const Header = ({ dispatch, username }) => (
+const Header = ({ SignOut, username }) => (
     <div className="App-header container-full">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title dn-m">Welcome</h1>
         <h4 className="dn-m">{username} !</h4>
         <div className="center-text routes">
             <NavLink to="/dashboard" activeClassName="active">App</NavLink>
-            <CustomButton onClick={() => dispatch(SignOut())}>
+            <CustomButton onClick={SignOut}>
                 Logout
             </CustomButton>
         </div>
@@ -28,10 +28,8 @@ const Header = ({ dispatch, username }) => (
     </div>
 )
 
-const mapStateToProps = ({ auth }) => {
-    return {
-        username: auth.name
-    }
-}
+const mapStateToProps = ({ auth }) => ({
+    username: auth.name
+})
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps, { SignOut )(Header)
